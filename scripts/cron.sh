4566d654a4e994d13eb36abed97db454843fd746
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2018-07-30 10:27:22 +0100

    Update

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,4 +1,10 @@
 # To add to cron job:
+set -euo pipefail
 
-bash add_run.sh $path >> runlog_upload_log.txt 2>&1
-bash add_nipt.sh >> runlog_upload_log.txt 2>&1
+if bash add_run.sh $path >> runlog_upload_log.txt 2>&1; then
+    :
+fi
+
+if bash add_nipt.sh >> runlog_upload_log.txt 2>&1; then
+    :
+fi
